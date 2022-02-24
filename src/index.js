5dc421f3f9679aaa0b26b3e57db211d0a5f9fce2
Vehx <vehx@protonmail.com>
2022-02-24 03:40:56 +0100

    added post route for api to send robot request to

--- src/index.js
+++ src/index.js
@@ -1,6 +1,42 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.port || 2222;
+const Grid = require('./grid');
+const Robot = require('./robot');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send({
+        message: 'We made it'
+    });
+});
+
+app.post('/', async (req, res) => {
+    const robot = new Robot(req.body.robot.x, req.body.robot.y, req.body.robot.face);
+    const grid = new Grid(req.body.grid.x, req.body.grid.y);
+
+    if (req.body.grid.obstacle) {
+        req.body.grid.obstacle.forEach(obstacle => {
+            grid.addObstacle(obstacle.x, obstacle.y);
+        });
+    };
+
+    const moved = await robot.move(req.body.robot.move.toString(), grid);
+    
+    if (moved) {
+        res.json({
+            isOutOfBounds: robot.isOutOfBounds,
+            ranIntoObstacle: robot.ranIntoObstacle,
+            facing: robot.face(),
+            position: robot.position()
+        });
+    };
+});
 
 app.listen(port, () => {
     const datetime = new Date();
@@ -8,8 +44,4 @@ app.listen(port, () => {
     // console.log(message);
 });
 
-app.get('/', (req, res) => {
-    res.send({
-        message: 'We made it'
-    });
-});
+module.exports = app;
